b67c47a45914f46cd6233bfbea6428f40e02a7bb
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-05 11:09:13 -0400

    Playing animation of cat down of 100px

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,7 +5,32 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  @override
+    void initState() {
+      super.initState();
+
+      catController = AnimationController(
+        duration: Duration(seconds: 2),
+        vsync: this,
+      );
+
+      catAnimation = Tween(
+        begin: 0.0,
+        end: 100.0,
+      ).animate(
+        CurvedAnimation(
+          parent: catController,
+          curve: Curves.easeIn,
+        ),
+      );
+
+      catController.forward();
+    }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,7 +44,17 @@ class _HomeState extends State<Home> {
   }
 
   Widget buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container (
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
+  
 
 }

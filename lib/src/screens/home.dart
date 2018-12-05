8ab590f0f679ad3037a7e4332f065e8a29a0343e
Miguel Bolivar <MBolivar360@gmail.com>
2018-12-05 10:32:23 -0400

    Adding initial cat rendering from Image.network.-

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+         appBar: AppBar(
+           title: Text('Animation'),
+         ),
+         body: buildAnimation(),
+       ),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
+
+}
